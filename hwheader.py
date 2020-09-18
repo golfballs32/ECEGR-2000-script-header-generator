9b6a675cb62fff5ad8f2bbea989754024362815c
golfballs32 <golfballs32@gmail.com>
2020-09-17 23:31:05 -0700

    Update hwheader.py

--- hwheader.py
+++ hwheader.py
@@ -35,50 +35,45 @@ lastName = "Redhawk"   #enter your last name
 import sys
 from datetime import date
 
-def main():
-    
-    argLen = len(sys.argv)  #check arguments passed by user
-    if(argLen != 4):
-        print("Invalid Arguments, please make arguments in the form of script assignment-number assignment-name")
-        exit()
-    
-    script = str(sys.argv[1]) #assign arguments to variables, get date
-    AS = str(sys.argv[2])
-    AN = str(sys.argv[3])
-    today = date.today()
-    d3 = today.strftime("%m/%d/%y")
-    
-    f = open(script, "r")   #ensure target script doesn't already have header
-    if("# File Name: " in f.readline()):
-        print("Header already in file")
-        exit()
-    f.seek(0)
-    oldText = f.read()
-    f.close()
-    
-    infile = open(script, "w") #write header to script
-    infile.seek(0)
-    infile.write("# File Name: " + lastName + "_" + firstName + "_AS" + AS + ".py\n")
-    infile.write("# File Path:  /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py\n")
-    infile.write("# Run Command: sudo python3 /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py\n")
-    infile.write("\n")
-    infile.write("# " + firstName + " " + lastName + "\n")
-    infile.write("# Date " + d3 + "\n")
-    infile.write("# AS." + AS + "\n")
-    infile.write("# " + AN + "\n" + "\n")
-    infile.write(oldText)
-    infile.close()
-    
-    print("\n# File Name: " + lastName + "_" + firstName + "_AS" + AS + ".py")
-    print("# File Path:  /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py")
-    print("# Run Command: sudo python3 /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py\n")
-    print("# " + firstName + " " + lastName)
-    print("# Date " + d3)
-    print("# AS." + AS)
-    print("# " + AN)
-    
-    print("\nDone!")
+argLen = len(sys.argv)  #check arguments passed by user
+if(argLen != 4):
+    print("Invalid Arguments, please make arguments in the form of script assignment-number assignment-name")
     exit()
 
-if __name__ == "__main__": #idk it doesn't work without this. try googling it
-    main()
+script = str(sys.argv[1]) #assign arguments to variables, get date
+AS = str(sys.argv[2])
+AN = str(sys.argv[3])
+today = date.today()
+d3 = today.strftime("%m/%d/%y")
+
+f = open(script, "r")   #ensure target script doesn't already have header
+if("# File Name: " in f.readline()):
+    print("Header already in file")
+    exit()
+f.seek(0)
+oldText = f.read()
+f.close()
+
+infile = open(script, "w") #write header to script
+infile.seek(0)
+infile.write("# File Name: " + lastName + "_" + firstName + "_AS" + AS + ".py\n")
+infile.write("# File Path:  /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py\n")
+infile.write("# Run Command: sudo python3 /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py\n")
+infile.write("\n")
+infile.write("# " + firstName + " " + lastName + "\n")
+infile.write("# Date " + d3 + "\n")
+infile.write("# AS." + AS + "\n")
+infile.write("# " + AN + "\n" + "\n")
+infile.write(oldText)
+infile.close()
+
+print("\n# File Name: " + lastName + "_" + firstName + "_AS" + AS + ".py")
+print("# File Path:  /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py")
+print("# Run Command: sudo python3 /home/python/userName/" + lastName + "_" + firstName + "_AS" + AS + ".py\n")
+print("# " + firstName + " " + lastName)
+print("# Date " + d3)
+print("# AS." + AS)
+print("# " + AN)
+
+print("\nDone!")
+exit()
